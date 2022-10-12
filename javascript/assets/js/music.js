97db70c4b7f30c577f4d0dc46c424d668610c882
kimdaewonn <109262186+kimdaewonn@users.noreply.github.com>
2022-10-12 21:20:22 +0900

    1012

--- javascript/assets/js/music.js
+++ javascript/assets/js/music.js
@@ -1,55 +1,55 @@
 const allMusic = [
     {
-        name : "1. Average",
-        artist : "Patrick Patrikios",
+        name : "1. Keep On Movin",
+        artist : "King Canyon",
         img: "music_view01",
         audio: "music_audio01"
     },
     {
-        name : "2. Bird Food",
-        artist : "DJ Freedem",
+        name : "2. Yes and No at the Same Time",
+        artist : "half.cool",
         img: "music_view02",
         audio: "music_audio02"
     },
     {
-        name : "3. Cat Shat in the Box",
-        artist : "josh pan",
+        name : "3. Next Steps",
+        artist : "half.cool",
         img: "music_view03",
         audio: "music_audio03"
     },
     {
-        name : "4. First Of 3",
-        artist : "Jeremy Black",
+        name : "4. Will 2 Pwr",
+        artist : "half.cool",
         img: "music_view04",
         audio: "music_audio04"
     },
     {
-        name : "5. Forget Me Not",
-        artist : "Patrick Patrikios",
+        name : "5. Insta Beat Vixens",
+        artist : "half.cool",
         img: "music_view05",
         audio: "music_audio05"
     },
     {
-        name : "6. Getaway Powder",
-        artist : "DJ Freedem",
+        name : "6. Sharp Edges",
+        artist : "half.cool",
         img: "music_view06",
         audio: "music_audio06"
     },
     {
-        name : "7. Nocturne",
-        artist : "Asher Fulero",
+        name : "7. Hey There",
+        artist : "half.cool",
         img: "music_view07",
         audio: "music_audio07"
     },
     {
-        name : "8. Our Planet",
-        artist : "Jeremy Black",
+        name : "8. Gemini",
+        artist : "half.cool",
         img: "music_view08",
         audio: "music_audio08"
     },
     {
-        name : "9. Pluckandplay",
-        artist : "Kwon",
+        name : "9. This Close",
+        artist : "half.cool",
         img: "music_view09",
         audio: "music_audio09"
     },
